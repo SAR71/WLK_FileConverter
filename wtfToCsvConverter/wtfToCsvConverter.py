d51f4c2326487bbef8900ed66bd38b55bd05f4b4
Victor Dienstbier <Victor@ArViCor.NET>
2020-06-14 14:20:19 +0200

    Anpassungsversuche zum Konvertieren der Dateien

--- wtfToCsvConverter/wtfToCsvConverter.py
+++ wtfToCsvConverter/wtfToCsvConverter.py
@@ -1,13 +1,22 @@
-from PyByteBuffer import ByteBuffer #https://pypi.org/project/PyByteBuffer/
-import pandas as pd
+#from PyByteBuffer import ByteBuffer #https://pypi.org/project/PyByteBuffer/
 import struct
-import sys
 
 in_file = open(r"D:\Temp\WeatherStation\2013-08.wlk", "rb")
 data = in_file.read()
 in_file.close()
 
+#content = data.decode('ansi').splitlines()
 content = data.decode('ansi', 'slashescape')
-print(content)
 
+#offset = 0
+#content = struct.unpack_from("<L", data, offset)
 
+with open(r"D:\Temp\WeatherStation\Output.txt", "w") as text_file:
+    for line in content:
+        text_file.write(line)
+
+#decoded = data.decode('ansi', 'slashescape')
+
+#offset = 0
+#content = struct.unpack_from("<d", data, offset)
+#data.decode('ansi', 'slashescape')
